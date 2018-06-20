b2a9cc47e0b468600e14ec978f4455cbec15cbfc
Jacob Moore <jmooree30@gmail.com>
2018-06-20 01:27:55 -0400

    API call to recieve breed data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  restItems: any;
+  restItemsUrl = 'https://breeds-a648.restdb.io/rest/dogs  ';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getRestItems();
+  }
+
+  // Read all REST Items
+  getRestItems(): void {
+    this.restItemsServiceGetRestItems()
+      .subscribe(
+        restItems => {
+          this.restItems = restItems;
+          console.log(this.restItems);
+        }
+      )
+  }
+
+  // Rest Items Service: Read all REST Items
+  restItemsServiceGetRestItems() {
+    const headers = new HttpHeaders()
+    .set("x-apikey", "5b29e3da46624c7b24444fd8")
+    .set("Content-Type", "application/json")
+    .set('cache-control': 'no-cache');
+    
+    return this.http
+      .get<any[]>(this.restItemsUrl,{headers})
+      .pipe(map(data => data));
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BreedsComponent } from './breeds/breeds.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreedsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
