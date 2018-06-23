a498874952f6fc7bc33c71c7653bfda9f1ced3e4
Jacob Moore <jmooree30@gmail.com>
2018-06-22 21:36:48 -0400

    Delete request functional

--- src/app/breeds/breeds.component.ts
+++ src/app/breeds/breeds.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { element } from 'protractor';
+import {RequestOptions, Request, RequestMethod} from '@angular/http';
 
 @Component({
   selector: 'app-breeds',
@@ -26,16 +27,26 @@ export class BreedsComponent implements OnInit {
           this.restItems = restItems;
       },
        (err) => console.log(err), () => {
-
          setTimeout(() => {
           let test = document.querySelectorAll('.del');
           test.forEach( (e) => {
             e.addEventListener('click', (element) => {
-              console.log(element.target.id);
+              this.http.delete('https://breeds-a648.restdb.io/rest/dogs' + '/' + element.target.id, { headers: new HttpHeaders()
+              .set("x-apikey", "5b2af49746624c7b2444501c")
+              .set("Content-Type", "application/json")
+              .set('cache-control', 'no-cache') })
+              .subscribe(
+                res => {
+                  window.location.href = '/index';
+                },
+                err => {
+                  alert("Error occured");
+                }
+              );
             })
           })
-         },1000)
-        }
+        },1000)
+    }
   }
 
   // Rest Items Service: Read all REST Items
